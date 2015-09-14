87e05e42b9ba0ee1ba94b1717ab58255b99a08a8
faizanayubi <indianayubi@gmail.com>
2015-09-14 23:35:24 +0530

    Added Basic Methods

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -5,7 +5,7 @@
  *
  * @author Faizan Ayubi
  */
-use Framework\Controller as Controller;
+use Shared\Controller as Controller;
 
 class Home extends Controller {
 
@@ -14,23 +14,59 @@ class Home extends Controller {
     }
     
     public function allproducts() {
-        
+        $this->seo(array(
+            "title" => "All Products",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function contact() {
-        
+        $this->seo(array(
+            "title" => "Contact Us",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function support() {
-        
+        $this->seo(array(
+            "title" => "Support",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function solutions() {
-        
+        $this->seo(array(
+            "title" => "Buisness Enterprise Solutions CloudStuffs Provide",
+            "view" => $this->getLayoutView()
+        ));
     }
     
     public function about() {
-        
+        $this->seo(array(
+            "title" => "Buisness Enterprise Solutions CloudStuffs Provide",
+            "view" => $this->getLayoutView()
+        ));
+    }
+    
+    public function hosting() {
+        $this->seo(array(
+            "title" => "Managed Hosting",
+            "view" => $this->getLayoutView()
+        ));
+    }
+    
+    public function onlineMarketing() {
+        $this->seo(array(
+            "title" => "Running Best Online Camapigns",
+            "view" => $this->getLayoutView()
+        ));
+    }
+    
+    public function professionalEmail() {
+        $this->seo(array(
+            "title" => "Get free professional Email",
+            "view" => $this->getLayoutView()
+        ));
     }
     
 }
