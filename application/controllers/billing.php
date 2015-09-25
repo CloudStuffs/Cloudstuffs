cd3119c88b862cab9f853130cc8a814d2a2e9e55
faizanayubi <indianayubi@gmail.com>
2015-09-25 10:05:34 +0530

    Added Blling Controller

---
+++ application/controllers/billing.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Description of billing
+ *
+ * @author Faizan Ayubi
+ */
+use Framework\Controller as Controller;
+use Framework\Registry as Registry;
+
+class Billing extends Controller {
+    //put your code here
+}
