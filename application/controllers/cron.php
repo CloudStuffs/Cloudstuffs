5c9c688a4249abdb78d6fc82d3202b6f8d519d30
Faizan Ayubi <indianayubi@gmail.com>
2015-11-01 22:16:03 +0530

    Added CRON Job

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -18,4 +18,41 @@ class CRON extends Auth {
         
     }
 
+    /**
+     * Generates New Bill at the end of period
+     * @return none none
+     */
+    protected function generateBills() {
+    	$now = strftime("%Y-%m-%d %H:%M:%S", strtotime('now'));
+    	$services = Service::all(array("live = ?" => true));
+
+    	foreach ($services as $service) {
+    		$period = date_diff(date_create($now), date_create($service->created));
+    		$timeline = date_diff(date_create($now), date_create($service->created));
+    		if ($timeline < $service->timeline) {
+    			if ($period == $service->period) {
+	    			$oldBill = Bill::first(array("id = ?" => $service->bill_id));
+	    			$bill = new Bill(array(
+	    				"service_id" => $service->id,
+	    				"amount" => $oldBill->amount,
+	    				"duedate" => date('Y-m-d', strtotime($now. ' + 15 days')),
+	    				"type" => $oldBill->type
+	    			));
+	    			$bill->save();
+
+	    			//send notification
+	    			
+	    		}
+    		}
+    	}
+    }
+
+    /**
+     * Send Notifications for any comment
+     * @return none none
+     */
+    protected function comment() {
+    	
+    }
+
 }
